cf9dcffb1896af6263f8ac59feed86574b155cfd
spidey-777 <spidey4952@gmail.com>
2025-03-16 15:50:11 +0530

    added velidater of signin or up middleware

---
+++ src/middlewares/index.js
@@ -0,0 +1,4 @@
+module.exports={
+    AuthValidaterRequest:require('./auth_request_velidater')
+
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const {AuthValidaterRequest} = require('../../middlewares/index')
 
 const UserContoller = require('../../controllers/user_controller');
 
 const router = express.Router();
 
 
-router.post('/signup',UserContoller.create)
+router.post('/signup',
+    AuthValidaterRequest.validateUserAuth,
+    UserContoller.create
+)
 
-router.post('/signin',UserContoller.signIn);
+router.post('/signin',
+    AuthValidaterRequest.validateUserAuth,
+    UserContoller.signIn
+);
 
 
 module.exports = router;
